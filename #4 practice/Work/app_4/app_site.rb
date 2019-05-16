a5cd3f6138cfa3160e77773011a36ee025f941d5
Ilya Semakin <il.semakin@yandex.ru>
2019-05-16 21:05:46 +0300

    links

---
+++ #4 practice/Work/app_4/app_site.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'sinatra'
+require_relative 'lib/mark'
+require_relative 'lib/diary'
+
+configure do
+    set :diary, Diary.new
+end
+
+get '/' do
+  erb :index
+end
+
+get '/mark' do
+  @mark = Mark.new('', '', 0)
+  erb :mark
+end
+
+get '/diary' do
+  @diary = settings.diary
+  erb :diary
+end
+
+post '/mark' do
+  @mark = Mark.new(params['name_course'], params['task'], params['mark'])
+  @errors = @mark.check_field
+
+  tmp = @errors.reject { |er| er == '' }
+  if tmp.empty?
+    settings.diary.add_mark(@mark)
+    redirect to('/mark')
+  else
+    erb :mark
+  end
+end
