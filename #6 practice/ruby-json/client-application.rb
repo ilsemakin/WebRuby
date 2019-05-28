b88c8653cc375f26a571dad76880e7addee26530
Ilya Semakin <il.semakin@yandex.ru>
2019-05-28 23:41:12 +0300

    ruby-json

---
+++ #6 practice/ruby-json/client-application.rb
@@ -0,0 +1,33 @@
+require 'net/http'
+require 'json'
+require 'pp'
+
+def app_url(path='')
+  URI("http://localhost:4567/#{path}")
+end
+
+def request_data
+  puts 'Sending GET-request'
+  response = Net::HTTP.get(app_url('/'))
+  puts "Got response: \n#{response}"
+end
+
+def send_data_to_server
+  puts 'Sending POST-request'
+  uri = app_url('/')
+  request = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json')
+  request.body = {param1: 'some value', param2: 'some other value'}.to_json
+  response = Net::HTTP.start(uri.hostname, uri.port) do |http|
+    http.request(request)
+  end
+  puts "Got response:\n#{response}"
+end
+
+def main
+  request_data
+  send_data_to_server
+end
+
+if $0 == __FILE__
+  main
+end
---
+++ #6 practice/ruby-json/web-application.rb
@@ -0,0 +1,14 @@
+require 'sinatra'
+require 'sinatra/json'
+require 'pp'
+
+get '/' do
+  json :foo => 'bar'
+end
+
+post '/' do
+  raw_body = request.body.read
+  puts "Raw request:\n#{raw_body}"
+  json_document = JSON.parse(raw_body)
+  puts "Parsed text:\n#{json_document}"
+end
