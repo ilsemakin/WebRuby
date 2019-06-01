bc2120099490c1cc6fbde860068df18187097088
Ilya Semakin <il.semakin@yandex.ru>
2019-06-02 02:39:50 +0300

    creating one apartment

---
+++ Laboratory/lib/apartment.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+# creating one apartment
+class Apartment
+  attr_reader :footage, :rooms, :address, :floor
+  attr_reader :type_of_house, :number_of_floors, :cost
+
+  def initialize(footage, rooms, address, floor, type_of_house, number_of_floors, cost)
+    @footage = footage.to_i
+    @rooms = rooms.to_i
+    @address = address
+    @floor = floor.to_i
+    @type_of_house = type_of_house
+    @number_of_floors = number_of_floors.to_i
+    @cost = cost.to_i
+  end
+
+  def check_fields
+    errors = {}
+
+    check_empty(errors)
+    check_negative(errors)
+
+    errors
+  end
+
+  private
+
+  def check_empty(errors)
+    message = 'This field cannot be empty!'
+
+    errors[:district] = message if @address.district.empty?
+    errors[:street] = message if @address.street.empty?
+  end
+
+  def check_negative(errors)
+    message = 'Number < 0!'
+
+    errors[:footage] = message if @footage <= 0
+    errors[:rooms] = message if @rooms <= 0
+    errors[:house] = message if @address.house <= 0
+    errors[:floor] = message if @floor <= 0
+    errors[:number_of_floors] = message if @number_of_floors <= 0
+    errors[:cost] = message if @cost <= 0
+  end
+end
