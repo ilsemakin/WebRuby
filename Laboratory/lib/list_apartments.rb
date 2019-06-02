e5aba1ab6f79f0685174b53166e9f09935a1eb83
Ilya Semakin <il.semakin@yandex.ru>
2019-06-03 00:26:33 +0300

    sorting

--- Laboratory/lib/list_apartments.rb
+++ Laboratory/lib/list_apartments.rb
@@ -13,4 +13,16 @@ class ListApartments
   def each
     @list.each { |apartment| yield apartment }
   end
+
+  def sort_by_footage
+    new_list = @list.sort_by(&:footage)
+
+    new_list
+  end
+
+  def sort_by_cost
+    new_list = @list.sort_by(&:cost)
+
+    new_list
+  end
 end
