b01faa75af905c809375e89a4a371bb432d032f1
Ilya Semakin <il.semakin@yandex.ru>
2019-06-12 02:55:49 +0300

    creating address

--- Laboratory/lib/address.rb
+++ Laboratory/lib/address.rb
@@ -2,11 +2,25 @@
 
 # creating address
 class Address
+  include Comparable
   attr_reader :district, :street, :house
 
   def initialize(district, street, house)
     @district = district
     @street = street
-    @house = house.to_i
+    @house = house
+  end
+
+  def ==(other)
+    @district == other.district &&
+      @street == other.street &&
+      @house == other.house
+  end
+
+  def to_s
+    str = "\n\tDistrict: #{@district}" \
+          "\n\tStreet: #{@street}" \
+          "\n\tHouse: #{@house}"
+    str
   end
 end
