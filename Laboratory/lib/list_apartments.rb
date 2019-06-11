3b4a4d67d90adbdf548bf597fbab3a6625af390b
Ilya Semakin <il.semakin@yandex.ru>
2019-06-12 02:57:52 +0300

    all functions

--- Laboratory/lib/list_apartments.rb
+++ Laboratory/lib/list_apartments.rb
@@ -1,34 +1,127 @@
 # frozen_string_literal: true
 
+require 'securerandom'
+require_relative 'checks'
+
 # creating list of apartments
 class ListApartments
+  include Check
   def initialize
-    @list = []
-  end
-
-  def add_apartment(apartment)
-    @list << apartment
+    @list = {}
   end
 
   def each
-    @list.each { |apartment| yield apartment }
+    @list.each { |key, apartment| yield key, apartment }
+  end
+
+  def each_value
+    @list.each_value { |apartment| yield apartment }
+  end
+
+  def empty?
+    @list.empty?
+  end
+
+  def [](key)
+    @list[key]
+  end
+
+  def add_apartment(apartment)
+    id = SecureRandom.uuid
+    @list.store(id, apartment)
+  end
+
+  def delete(key)
+    @list.delete(key)
   end
 
   def sort_by_footage
-    new_list = @list.sort_by(&:footage)
-
-    new_list
+    @list.sort_by { |_key, apartment| apartment.footage }.to_h
   end
 
   def sort_by_cost
-    new_list = @list.sort_by(&:cost)
-
-    new_list
+    @list.sort_by { |_key, apartment| apartment.cost }.to_h
   end
 
   def sort_by_district
-    new_list = @list.sort_by { |apartment| apartment.address.district }
+    @list.sort_by { |_key, apartment| apartment.address.district }.to_h
+  end
 
-    new_list
+  def find_swaps(this_apartment)
+    list_swaps = {}
+    @list.each do |key, apartment|
+      next unless Check.swap(this_apartment, apartment) &&
+                  Check.swap(apartment, this_apartment) &&
+                  this_apartment != apartment
+
+      list_swaps.store(key, apartment)
+    end
+
+    list_swaps
+  end
+
+  def find_swaps_street(this_apartment)
+    list_swaps = {}
+    @list.each do |key, apartment|
+      next unless this_apartment.address.street == apartment.address.street &&
+                  Check.swap(this_apartment, apartment) &&
+                  Check.swap(apartment, this_apartment) &&
+                  this_apartment != apartment
+
+      list_swaps.store(key, apartment)
+    end
+
+    list_swaps
+  end
+
+  def no_swaps
+    list_no_swaps = []
+    @list.each_value do |apartment|
+      list_no_swaps << apartment if find_swaps(apartment).empty?
+    end
+
+    list_no_swaps
+  end
+
+  def statistics
+    statistics = {}
+    districts.each do |district|
+      count = 0
+      swap = 0
+      average = 0
+      @list.each_value do |apartment|
+        next unless district == apartment.address.district
+
+        count += 1
+        swap += 1 if apartment.swap.list_districts.include?(district)
+        average += apartment.cost / apartment.footage
+      end
+      item = [count, swap, average / count]
+      statistics.store(district, item)
+    end
+
+    statistics
+  end
+
+  def check_database
+    error = {}
+    @list.each_value do |apartment1|
+      list = [apartment1]
+      @list.each_value do |apartment2|
+        list << apartment2 if Check.address(apartment1, apartment2, error)
+      end
+      error.store(apartment1.address, list) if list.size != 1
+    end
+
+    error
+  end
+
+  private
+
+  def districts
+    districts = []
+    @list.each_value { |apartment| districts << apartment.address.district }
+
+    districts.uniq
   end
 end
