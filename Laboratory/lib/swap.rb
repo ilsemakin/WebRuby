0c3e669b847b7f85e12ea2c22b9889f47221984a
Ilya Semakin <il.semakin@yandex.ru>
2019-06-19 17:40:33 +0300

    checks

--- Laboratory/lib/swap.rb
+++ Laboratory/lib/swap.rb
@@ -14,15 +14,6 @@ class Swap
     @range_cost = range_cost
   end
 
-  def check_fields
-    errors = {}
-
-    check_range(errors, @range_footage, @range_rooms, @range_cost)
-    check_floors(errors)
-
-    errors
-  end
-
   def to_s
     str = "\n\tFootage: #{@range_footage}" \
           "\n\tRooms: #{@range_rooms}" \
@@ -30,17 +21,5 @@ class Swap
           "\n\tFloors: #{@list_floors.join(', ')}" \
           "\n\tCost: #{@range_cost}"
     str
-  end
-
-  private
-
-  def check_range(errors, *ranges)
-    message = 'Check range!'
-    ranges.each { |range| errors.store(range, message) if Check.range(range) }
-  end
-
-  def check_floors(errors)
-    message = 'Invalid floors!'
-    errors[:list_floors] = message if Check.floors(@list_floors)
   end
 end
